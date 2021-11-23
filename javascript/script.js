2658284c46d113dce6fd63ab2e0f4307900ffa20
GreenNovaStar <brianyip5@gmail.com>
2021-11-23 10:56:14 -0800

    reverting changes

--- javascript/script.js
+++ javascript/script.js
@@ -22,7 +22,6 @@ var game = new Phaser.Game(config);
 
 //phaser asset url : https://labs.phaser.io/assets/
 function preload() {
-	//test preload
 	// this.load.setBaseURL("https://labs.phaser.io");
 	this.load.setPath("../assets");
 	// this.load.image("ball", "assets/sprites/spikedball.png");
