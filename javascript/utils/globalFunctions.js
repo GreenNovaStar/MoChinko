4d6a5a2fa5a4fa3552cdc96ada6ebc0678a817b7
GreenNovaStar <brianyip5@gmail.com>
2021-12-01 19:40:46 -0800

    added a function to add score to leaderboard, a temporary button is on the leaderboard scene

--- javascript/utils/globalFunctions.js
+++ javascript/utils/globalFunctions.js
@@ -30,3 +30,61 @@ function createCloseButton(game, sceneName) {
 	});
 	closeButton.on("pointerover", () => {});
 }
+
+function addScoreToLeaderboard(gameIndex, score) {
+	var selectedGameScores = gameScores[gameIndex];
+	if (gameScores[gameIndex].length < 10) {
+		gameScores[gameIndex].push(score);
+		gameScores[gameIndex].sort(function (a, b) {
+			return b - a;
+		});
+	} else {
+		gameScores[gameIndex][9] = score;
+		gameScores[gameIndex].sort(function (a, b) {
+			return b - a;
+		});
+	}
+}
+
+/*
+var storeBestTimes = function() {
+  let currentTime = minutes * 6000 + seconds * 100 + milliseconds;
+  if (bestTimes[test].length < 3) {
+    bestTimes[test].push([currentTime]);
+    bestTimes[test].sort(function(a, b) {
+      return a - b;
+    });
+  } else if (bestTimes[test][2] > 0 && bestTimes[test][2] != -1) {
+    console.log(
+      "minutes = " + minutes < 10
+        ? "0" + minutes
+        : minutes + "\nseconds = " + seconds < 10
+        ? "0" + seconds
+        : seconds + "\nmilli = " + milliseconds < 10
+        ? "0" + milliseconds
+        : milliseconds
+    );
+    if (bestTimes[test].length > 2) {
+      let time = bestTimes[test][2];
+
+      if (currentTime < time) {
+        bestTimes[test][2] = currentTime;
+        bestTimes[test].sort(function(a, b) {
+          return a - b;
+        });
+      }
+    }
+  } else {
+    bestTimes[test][2] = currentTime;
+    bestTimes[test].sort(function(a, b) {
+      return a - b;
+    });
+  }
+
+  // else{
+  //   bestTimes[test].push([currentTime]);
+  //   bestTimes[test].sort(function(a, b){return a - b});
+  // }
+  fillLeaderBoard();
+  console.log(bestTimes);
+};*/
